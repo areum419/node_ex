a4cc1ea029d09092460ad268b376ff1cee1acd63
areumm <areumm914@gmail.com>
2023-05-13 10:08:53 +0900

    add product model

---
+++ models/product.js
@@ -0,0 +1,25 @@
+// 1
+import mongoose from "mongoose";
+
+// 2
+const productsSchema = mongoose.Schema(
+    // 5
+    {
+        title: String,          // 제품명
+        price: Number,          // 제품가격
+        description: String,    // 제품설명
+        brand: String,          // 브랜드
+        company: String,        // 제조기업
+        stock: Number          // 재고량
+    } ,
+    {
+        timestamps: true
+    }
+);
+
+// 3
+const productModel = productsSchema.model("product", productsSchema); //테이블명 , 스카마(db항목들)
+
+
+// 4
+export default productModel;
