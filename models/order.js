8bf4c6f84c0fd9e09db8fa7794747b0beb4f9070
areumm <areumm914@gmail.com>
2023-05-13 12:22:45 +0900

    order CRUD 까지 전체 다 확인해야됨

--- models/order.js
+++ models/order.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const orderSchema = mongoose.Schema(
     {
         //구매하고자 하는 대상
-        //ref는 어느 테이블에잇는 오브젝트 형태인지
+        //ref는 어느 테이블에잇는 오브젝트 형태인지 (테이블명)
+        // type: mongoose.Schema.Types.ObjectId, >>>>>>> PK 값으로 지정해주는 의미임
         product:{
             type: mongoose.Schema.Types.ObjectId,
             ref: "product",
--- routes/order.js
+++ routes/order.js
@@ -2,9 +2,19 @@ import express from "express";
 import orderModel from "../models/order.js";
 const router = express.Router();
 
-router.get("/", (req,res) => {
+
+// order 전체 데이터 가져옴
+router.get("/", async (req,res) => {
+
+    const orders= await orderModel
+        .find() // 전체조회
+   //     .populate("product", ["title", "price"]) // 이건 title 이랑 price만 조회함
+        .populate("product") // 이건 title 이랑 price만 조회함 (위아래 차이점 다시확인)
+
+
     res.json({
-        msg :"order get all"
+        msg :"order get all",
+        orders
     })
 })
 
