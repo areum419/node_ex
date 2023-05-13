832758684460a3479d2e484587beb8b8f9057aac
areumm <areumm914@gmail.com>
2023-05-13 11:05:55 +0900

    전체조회 상세조회

--- routes/product.js
+++ routes/product.js
@@ -3,11 +3,50 @@ import product from "../models/product.js";
 import productModel from "../models/product.js";
 const router = express.Router();
 
-router.get("/", (req,res) => {
+router.get("/", async (req,res) => {
+
+    const products = await productModel.find(); // productModel에있는걸 찾겠다
+
     res.json({
-        msg :"product get all"
+        msg :"product get all",
+        count: products.length,
+      //  products:products,
+        products: products.map(item => {
+            return {
+                id: item._id,
+                title: item.title,
+                price: item.price
+            }
+        }) // arry 데이터를 한번에 뿌려줄때 map이라는 함수씀
     })
 })
+
+// 상세데이터 가져오는 api
+router.get("/:productId", async (req,res) => {
+
+    const product = await productModel.findById(req.params.productId);
+
+    if (!product){
+      res.json({
+          msg: "no product"
+      })
+    }else{
+        res.json({
+            msg: `successful get ${req.params.productId}$`,
+            product: product
+        })
+    }
+
+})
+
+
+
+
+
+
+
+
+
 
 router.post("/", async(req,res) => {
 
