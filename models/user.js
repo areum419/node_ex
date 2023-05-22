20c30b8e63068df18f4576a4cb154f0bc774cd51
areumm <areumm914@gmail.com>
2023-05-22 20:37:39 +0900

    회원가입 post 메소드 생성

--- models/user.js
+++ models/user.js
@@ -1,7 +1,29 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
-    {},
+
+    // 회원가입 할때 필요하는 정보
+    // id, pw, email, pn, adress
+    {
+        email : {
+            type: String,
+            required: true,
+            unique: true
+        }, // required 필수값인지 여부
+        username: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: Number
+        },
+        phone: {
+           type: Number
+        },
+        adress: {
+            type: String
+        }
+    },
     {
         timestamps: true
     }
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,41 @@ import userModel from "../models/user.js";
 
 const router = express.Router();
 
+// 회원가입 (데이터 등록)
+router.post("/", async (req, res) => {
+
+    // 사용자 입력값
+    const {username, password, email, phone, adress} =req.body;
+
+    const newUser = new userModel({
+        username, password, email, phone, adress
+    })
+
+    const createdUser = await newUser.save();
+
+    res.json({
+        msg: "create & user",
+        user: createdUser
+    })
+})
+
+// 로그인은 post 사용
+router.post("/login", async (req, res) => {
+
+    res.json({
+        msg: "Login"
+    })
+})
+
+
+//개인정보조회
+router.get("/", async (req,res) => {
+
+    res.json({
+        msg: "select"
+    })
+})
+
 
 
 
